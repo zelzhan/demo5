b444d6177fb8400431563afc6f5507af36795f8a
zelzhan <elzhan.zeinulla@nu.edu.kz>
2017-06-23 12:25:23 +0600

    fourth commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  helper_method :current_order
+
+  def current_order
+    if !session[:order_id].nil?
+      Order.find(session[:order_id])
+    else
+      Order.new
+    end
+  end
+
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'order_items/create'
+
+  get 'order_items/update'
+
+  get 'order_items/destroy'
+
+  get 'carts/show'
+
+  get 'products/index'
+
   devise_for :users
   root 'home#index'
 
